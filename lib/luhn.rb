f8cd397ea6d5b2810daaeaf127e4357f4c02de5c
Wilson <wphoong@gmail.com>
2017-06-10 07:15:00 +0000

    refactored code

--- lib/luhn.rb
+++ lib/luhn.rb
@@ -1,67 +1,56 @@
 require 'benchmark'
 
-
 def is_valid?(number)
-
   double = []
-  underTen = []
+  under_ten = []
   summation = 0
 
   # takes input, turns into a string and reverses it
   reverse = number.to_s.split('').reverse
 
-  #Double every even number
-    #iterate through reversed string, doubling every second digit
+  # Double every even number
+  # iterate through reversed string, doubling every second digit
+  # checks if index is even and is not the last item
+  # if true, double and push onto double array, else push onto double array
   reverse.each_with_index do |dub, index|
-    #checks if index is even and is not the last item
-    index %2 ==0 && index != reverse.length - 1 ? 
-    #if true, double and push onto double array, else push onto double array
-    double << dub.to_i * 2 : double << dub.to_i
+    double << if index.even? && index != reverse.length - 1
+                dub.to_i * 2
+              else
+                dub.to_i
+              end
   end
 
-  #If the doubled value is greater than or equal to 10, take the value and subtract 9 from it.
-  #iterate through double array
+  # If the doubled value is greater than or equal to 10, take the value and subtract 9 from it.
+  # iterate through double array
+  # check if each number is >= 10
+  # if true, subtract 9 and push onto underTen array,
+  # else push onto underTen array
   double.each do |x|
-    #check if each number is >= 10
-      #if true, subtract 9 and push onto underTen array,
-        #else push onto underTen array
-    x >= 10 ? underTen << x - 9 : underTen << x
+    under_ten << (x >= 10 ? x - 9 : x)
   end
 
-  #Sum the digits
-  #iterate through underTen array and add all numbers
-  underTen.each do |sum|
-    # store into summation variable
-    summation +=sum
+  # Sum the digits
+  # iterate through underTen array and add all numbers
+  # store into summation variable
+  under_ten.each do |sum|
+    summation += sum
   end
 
   puts "Orginal sequence: #{number}"
   puts "Reversed sequence: #{reverse.join}"
   puts "Double: #{double.join}"
-  puts "Under Ten: #{underTen.join}" 
+  puts "Under Ten: #{underTen.join}"
   puts "Sum: #{summation}"
-  #If the sum is divisible by 10 it's a valid number. Otherwise it's invalid.
-  puts summation % 10==0 ? true :  false
-
+  # If the sum is divisible by 10 it's a valid number. Otherwise it's invalid.
+  puts summation % 10 == 0 ? true : false
 end
 
-is_valid?(4194560385008504)
+is_valid?(4_194_560_385_008_504)
 
-
-#Run a benchmark
+# Run a benchmark
 
 # number = 4194560385008504
 
 # Benchmark.bm do |x|
 #   x.report("is_valid?") { is_valid?(number) }
-
 # end
-
-
-
-
-
-
-
-
-
--- spec/luhn_spec.rb
+++ spec/luhn_spec.rb
@@ -1,27 +1,21 @@
 require 'spec_helper'
 
 RSpec.describe Luhn, type: :model do
-
   describe 'is_valid? should work' do
-
-    it "should return true if the number is valid" do
-      expect(Luhn.is_valid?(4194560385008504)).to be true
+    it 'should return true if the number is valid' do
+      expect(Luhn.is_valid?(4_194_560_385_008_504)).to be true
     end
 
-    it "should return false if the number is not valid" do
-
-      expect(Luhn.is_valid?(4194560385008504)).to be false
+    it 'should return false if the number is not valid' do
+      expect(Luhn.is_valid?(4_194_560_385_008_504)).to be false
     end
 
-    it "should give the wrong answer if you begin on the left side for valid numbers" do
-      expect(Luhn.is_valid?(377681478627336)).to be true
+    it 'should give the wrong answer if you begin on the left side for valid numbers' do
+      expect(Luhn.is_valid?(377_681_478_627_336)).to be true
     end
 
-    it "should give the wrong answer if you begin on the left for invalid numbers" do 
-      expect(Luhn.is_valid?(377681478627337)).to be false
+    it 'should give the wrong answer if you begin on the left for invalid numbers' do
+      expect(Luhn.is_valid?(377_681_478_627_337)).to be false
     end
-
   end
-
-
 end
